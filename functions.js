52396d3c5585ca084d383235adf0d3ef66c60f16
Elvis Stiglic <41340032+NukeTurtle@users.noreply.github.com>
2021-04-25 22:16:26 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
+const totalBill = total_bill;
+function billCost() {
+    totalBill.innerHTML = bill.value;
+}
+ 
+tip_percent.oninput = function () {
+    percent_number.innerHTML = this.value;
+}
 
-
-
-
-
-
+tip_split.oninput = function () {
+    split_number.innerHTML = this.value;
+}
